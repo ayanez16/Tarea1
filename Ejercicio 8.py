b0975413a5c866479a43673402c55ce83f524b7f
ayanez16 <86695767+ayanez16@users.noreply.github.com>
2021-12-25 22:16:48 -0500

    Update Ejercicio 8.py

--- Ejercicio 8.py
+++ Ejercicio 8.py
@@ -1,4 +1,4 @@
-#Construye un algoritmo que calcule e imprima la tabla de multiplicar, desde la tabla
+#Imprimir la tabla de multiplicar, desde la tabla
 #del 1 hasta la del 10.
 numero = int(input("Ingrese el numero de la tabla que desea presentar: "))
 for i in range(1,11):
