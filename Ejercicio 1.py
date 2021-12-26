a5550bbe6daa9f5be685f42aa8cb21282f130347
ayanez16 <86695767+ayanez16@users.noreply.github.com>
2021-12-25 22:05:08 -0500

    Update Ejercicio 1.py

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,6 +1,4 @@
-#Dado un número entero N, calcular e informar al usuario cuántos dígitos tiene
-#dicho número.
+#Digitos de un numero
 num = int(input("Porfavor, ingrese un numero: "))
 contador = len(str(num))
-
 print("El numero ingresado tiene "+str(contador) +" digitos")
