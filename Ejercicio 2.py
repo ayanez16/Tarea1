8fd4d7ba13230a690f949c2b0b954afb1722e5a4
ayanez16 <86695767+ayanez16@users.noreply.github.com>
2021-12-25 22:06:14 -0500

    Update Ejercicio 2.py

--- Ejercicio 2.py
+++ Ejercicio 2.py
@@ -1,4 +1,4 @@
-#Dado un número, determine si es capicúa.
+#Determine si un numero es capicua.
 numero = int(input("Ingrese un numero: "))
 if str(numero) == str(numero) [:: -1]:
     print("El numero si es capicua")
